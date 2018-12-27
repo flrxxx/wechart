b3ceebbd9ea906a12e3c4002ee61644b7cc5f304
flrxxx <flrxxx@qq.com>
2018-12-27 16:51:58 +0800

    拼团页面补充

---
+++ pages/join_competition/join_competition.js
@@ -0,0 +1,19 @@
+Page({
+  data:{
+    bannerbg:"../../images/bannerbg.png",
+    useimg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545904706273&di=162ecfef50c1f972a9eeb823e20c4fbd&imgtype=0&src=http%3A%2F%2Fwww.jituwang.com%2Fuploads%2Fallimg%2F151003%2F258203-1510030RP894.jpg",
+    usename:"刘若曦",
+    hasnum:'1',
+    otherinfo:[
+      {
+      userimg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545904706273&di=162ecfef50c1f972a9eeb823e20c4fbd&imgtype=0&src=http%3A%2F%2Fwww.jituwang.com%2Fuploads%2Fallimg%2F151003%2F258203-1510030RP894.jpg",
+      username:'人寿刘总'
+      }, {
+        userimg: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545904706273&di=162ecfef50c1f972a9eeb823e20c4fbd&imgtype=0&src=http%3A%2F%2Fwww.jituwang.com%2Fuploads%2Fallimg%2F151003%2F258203-1510030RP894.jpg",
+        username: '李淼淼李淼淼李淼淼'
+      }
+    ],
+    lasttime:'1天23小时10分32秒',
+    contenttext: "<p class='contenttext'>点亮您的个人品牌</p><p class='contenttext'>打造自己的自媒体平台</p><p class='contenttext'>让专业服务更有温度</p><p class='contenttext'>经营高端客户的服务利器</p>"
+  }
+})
