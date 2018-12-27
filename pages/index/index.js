0164613b202b13b7687ab9a8da765c771b2c80b7
flrxxx <flrxxx@qq.com>
2018-12-27 14:44:00 +0800

    开灯app

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,14 +10,10 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     h1text:"<p class='titletext'>一起拼团 即刻开通</p>",
     boldtext:"<p class='titletext boldtext'>开灯CRM管理系统</p>",
-    cardimg:'../../images/card.png'
+    cardimg:'../../images/card.png',
+    contenttext: "<p class='contenttext'>点亮您的个人品牌</p><p class='contenttext'>打造自己的自媒体平台</p><p class='contenttext'>让专业服务更有温度</p><p class='contenttext'>经营高端客户的服务利器</p>"
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+ 
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
