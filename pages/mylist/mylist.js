b70cc91de8de2b21afda7a1cd70d914d4955052a
flrxxx <flrxxx@qq.com>
2018-12-29 16:55:52 +0800

    开灯其他页面

---
+++ pages/mylist/mylist.js
@@ -0,0 +1,67 @@
+// pages/mylist/mylist.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    useimg: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545904706273&di=162ecfef50c1f972a9eeb823e20c4fbd&imgtype=0&src=http%3A%2F%2Fwww.jituwang.com%2Fuploads%2Fallimg%2F151003%2F258203-1510030RP894.jpg",
+    usename: "刘若曦",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
